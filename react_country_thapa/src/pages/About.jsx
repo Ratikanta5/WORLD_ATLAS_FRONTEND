7611bd50b78ab07b370c271d9e1aa134cbed0791
Ratikanta <ratikantamohanty924@gmail.com>
2025-11-24 21:08:58 +0530

    about page completion

--- react_country_thapa/src/pages/About.jsx
+++ react_country_thapa/src/pages/About.jsx
@@ -1,7 +1,42 @@
-export default function Home(){
-    return(
-        <>
-            <h1>About</h1>
-        </>
-    )
+import countryFact from "../api/countryData.json";
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the Interesting Facts
+        <br />
+        we’re proud of
+      </h2>
+
+      <div className="gradient-cards">
+        {countryFact.map((country) => {
+            const {id, countryName, capital, population,interestingFact  } = country;
+          return (
+            <div className="card" key={id}>
+              <div className="container-card bg-blue-box">
+                <p className="card-title">{countryName}</p>
+                <p>
+                  <span className="card-description">capital:</span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Fact:</span>
+                    {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
+
+export default About
